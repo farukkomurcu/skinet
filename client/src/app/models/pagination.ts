0076a57eb7c2e958108f72f37aead6add540b4d8
notfaruk <kmrcfaruk@gmail.com>
2022-05-23 01:17:33 +0300

    End of section 8

---
+++ client/src/app/models/pagination.ts
@@ -0,0 +1,16 @@
+import { IProduct } from "./product";
+
+    export interface IPagination {
+        data: IProduct[];
+        pageIndex: number;
+        pageSıze: number;
+        count: number;
+        daya: IProduct[];
+
+    }
+
+
+    
+
+
+
---
+++ client/src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct{
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    picturerl: string;
+    productType:string;
+    productBrand: string;
+}
+
