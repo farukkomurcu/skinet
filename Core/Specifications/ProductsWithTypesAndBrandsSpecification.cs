8de65cd000baafa9f79fc5366d9a2ab6e772c63c
notfaruk <kmrcfaruk@gmail.com>
2022-06-06 19:36:07 +0300

    End of section 11

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -8,7 +8,9 @@ namespace Core.Specifications
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
             : base(x =>
                 (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
-                (productParams.Search))
+                (productParams.Search))&&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
               
 
             )
